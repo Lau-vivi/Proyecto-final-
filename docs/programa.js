e4736d27036482c840bf2ea4f8d112f6184e859e
Laura Viviana Ramirez Borda <lvramirezb@udistrital.edu.co>
2025-04-30 22:01:02 -0500

    v0.11

--- docs/programa.js
+++ docs/programa.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([4.555736, -74.084008], 13);
+var map = L.map('map').setView([4.555742937351999, -74.08400870501436], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
